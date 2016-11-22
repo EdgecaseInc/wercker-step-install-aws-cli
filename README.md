9d565465c07d400f2317116b870bb0fa9b85b75e
Nick den Engelsman <nickdenengelsman@gmail.com>
2016-11-22 09:48:20 +0100

    Add version option and updated README.md

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
-install-aws-cli
-===========================
+# install-aws-cli
 
-Install AWS Command Line Tools
+Installs the AWS Command Line Tools
 
+Based on [AWS CLI installation](http://docs.aws.amazon.com/cli/latest/userguide/installing.html).
 
-Example
---------
+## Options
 
-Add AWS_KEY and AWS_SECRET as deploy target or application environment variables.
-`region` is optional.
+- `key` required
+- `secret` required
+- `region` optional
+- `version` optional, defaults to latest
 
-```
-    - edgecaseadmin/install-aws-cli:
-        key: $AWS_KEY
-        secret: $AWS_SECRET
-        region: $AWS_REGION
+### Example
+
+```yaml
+steps:
+  - edgecaseadmin/install-aws-cli:
+      key: $AWS_KEY
+      secret: $AWS_SECRET
+      region: $AWS_REGION
+      version: 1.11.18
 ```
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ ! -n "$WERCKER_INSTALL_AWS_CLI_KEY" ]; then
 	error 'Please specify key property'
 	exit 1
@@ -11,9 +13,15 @@ if [ ! -n "$WERCKER_INSTALL_AWS_CLI_SECRET" ]; then
 fi
 
 if [ ! -n "$WERCKER_INSTALL_AWS_CLI_REGION" ]; then
-  echo '[WARN] No Region Specified'
+  echo '[WARN] No region specified'
 fi
 
+if [ ! -n "$WERCKER_INSTALL_AWS_CLI_VERSION" ]; then
+	echo '[WARN] No version specified, going with latest'
+  WERCKER_INSTALL_AWS_CLI_VERSION=""
+else
+	WERCKER_INSTALL_AWS_CLI_VERSION="-$WERCKER_INSTALL_AWS_CLI_VERSION"
+fi
 
 echo 'Synchronizing References in apt-get...'
 sudo apt-get update
@@ -21,16 +29,19 @@ sudo apt-get update
 echo 'Installing Unzip...'
 sudo apt-get install unzip -y
 
+echo 'Changing directory to /tmp...'
+cd /tmp
+
 echo 'Grabbing AWS CLI...'
-wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
-unzip awscli-bundle.zip
+wget "https://s3.amazonaws.com/aws-cli/awscli-bundle$WERCKER_INSTALL_AWS_CLI_VERSION.zip"
+unzip "awscli-bundle$WERCKER_INSTALL_AWS_CLI_VERSION.zip"
 
 echo 'Installing AWS CLI...'
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 rm -rf awscli-bundle*
 
 echo 'Synchronizing System Time...'
-sudo ntpdate ntp.ubuntu.com
+sudo ntpdate ntp.ubuntu.com || true
 
 echo 'Configuring based on parameters...'
 aws configure set aws_access_key_id $WERCKER_INSTALL_AWS_CLI_KEY
